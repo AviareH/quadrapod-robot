adbb893d3ec6cdf0d419b51c313aa6006deaca7b
AviareH <avih2018@gmail.com>
2026-07-02 14:05:58 -0400

    Changed code to work with PCA9685 PWM Driver

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,48 +1,56 @@
 #include <Arduino.h>
 #include <ESP32Servo.h> 
+#include <Adafruit_PWMServoDriver.h>
 
-Servo FRfoot; //Front right foot
-Servo FRknee; //Front right knee 
-Servo FRhip; //Front right hip
-Servo FLfoot;
-Servo FLknee;
-Servo FLhip;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-int FRfootPin = 17; 
-int FRkneePin = 18;
-int FRhipPin = 19;
-int FLfootPin = 16;
-int FLkneePin = 4;
-int FLhipPin = 2;
+#define SERVOMIN 130  
+#define SERVOMAX 520 
+
+int FLFoot=0; //starts at 0, max at 180
+int FLKnee=1; //starts at 180, max at 15
+int FLHip=2; // starts at 0, max at 150
+
+int FRFoot=4; //starts at 0, max at 180
+int FRKnee=5; //starts at 180, max at 15
+int FRHip=6; // starts at 180, max at 30
+
+int BRFoot=8; //starts at 0, max at 180
+int BRKnee=9; //starts at 180, max at 15
+int BRHip=10; // starts at 0, max at 150
+
+int BLFoot=12; //starts at 0, max at 180
+int BLKnee=13; //starts at 180, max at 15
+int BLHip=14; // starts at 180, max at 30
+
+
+void setServoAngle(uint8_t channel, float angle) {
+  float pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(channel, 0, pulse);
+}
 
 void neutral(){
-  FRfoot.write(180);
-  FRknee.write(0);
-  FRhip.write(180);
-
-  FLfoot.write(0);
-  FLknee.write(180);
-  FRhip.write(0);
+  setServoAngle(FLFoot, 0);
+  setServoAngle(FLKnee, 180);
+  setServoAngle(FLHip, 0);
+  setServoAngle(FRFoot, 0);
+  setServoAngle(FRKnee, 0);
+  setServoAngle(FRHip, 180);
 }
 
 void setup() {
-  FRfoot.attach(FRfootPin); //180 default (max) 10 (min)
-  FRknee.attach(FRkneePin); //0 default (min) 150 (max)
-  FRhip.attach(FRhipPin); //180 default (max) 30 (min)
-
-  FLhip.attach(FLhipPin);
-  FLknee.attach(FLkneePin);
-  FLfoot.attach(FLfootPin);
+  Serial.begin(115200);
+  pwm.begin();
+  pwm.setPWMFreq(50); 
+  delay(10);
+  
   neutral();
+  delay(2000); 
 }
 
 void loop() {
-  //FLhip.write(90);
-  //FLfoot.write(90);
-  FLknee.write(90);
+  setServoAngle(BLFoot, 0);
   delay(1000);
-  //FLfoot.write(0);
-  FLknee.write(180);
-  //FLhip.write(0);
+  setServoAngle(BLFoot, 159);
   delay(1000);
 }

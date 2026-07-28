d9e9634e1862d0cd99a37bb65b8a51d54c99cc89
AviareH <avih2018@gmail.com>
2026-07-28 00:11:32 -0400

    Added inverse kinematics math

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,33 +1,70 @@
 #include <Arduino.h>
 #include <ESP32Servo.h> 
 #include <Adafruit_PWMServoDriver.h>
+#include <math.h>
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVOMIN 130
 #define SERVOMAX 520
 
-int FLFoot=0; //starts at 0, max at 180
-int FLKnee=1; //starts at 180, max at 15
-int FLHip=2; // starts at 0, max at 150
+const float KneeLink = 61.5;
+const float FootLink = 75.5;
+const float HipOffset = 31.5;
 
-int FRFoot=4; //starts at 0, max at 180
-int FRKnee=5; //starts at 180, max at 15
-int FRHip=6; // starts at 180, max at 30
+struct Servo {
+  uint8_t channel;
+  float angle;
+};
 
-int BRFoot=8; //starts at 0, max at 180
-int BRKnee=9; //starts at 180, max at 15
-int BRHip=10; // starts at 0, max at 150
+struct Point {
+  float x;
+  float y;
+  float z;
+};
 
-int BLFoot=12; //starts at 0, max at 180
-int BLKnee=13; //starts at 180, max at 15
-int BLHip=14; // starts at 180, max at 30
+struct JointAngles {
+  float theta1; // Hip
+  float theta2; // Knee
+  float theta3; // Foot
+};
+
+Servo FLFoot = {0, 0};
+Servo FLKnee = {1, 180};
+Servo FLHip  = {2, 0};
+
+Servo FRFoot = {4, 0};
+Servo FRKnee = {5, 180};
+Servo FRHip  = {6, 180};
+
+Servo BRFoot = {8, 0};
+Servo BRKnee = {9, 180};
+Servo BRHip  = {10, 0};
+
+Servo BLFoot = {12, 0};
+Servo BLKnee = {13, 180};
+Servo BLHip  = {14, 180};
 
 
-void setServoAngle(uint8_t channel, float angle) {
-  float pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
-  pwm.setPWM(channel, 0, pulse);
+void setServoAngle(Servo &servo, float angle) {
+  servo.angle = constrain(angle, 0.0f, 180.0f);
+  uint16_t pulse = map((int)round(servo.angle), 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(servo.channel, 0, pulse);
 }
+
+JointAngles SolveIK(const Point& target){
+  JointAngles angles;
+
+  float d = sqrt(pow(target.x,2)+pow(target.y,2)) - HipOffset;
+  float c = sqrt(pow(d,2)+pow(target.z,2));
+
+  angles.theta1 = (atan2(target.x,target.y))*180/M_PI;
+  angles.theta2 = (atan2(d,target.z)+acos((pow(KneeLink,2)+pow(c,2)-pow(FootLink,2))/(2.0*KneeLink*c)))*180/M_PI;
+  angles.theta3 = (acos((pow(KneeLink,2)-pow(c,2)+pow(FootLink,2))/(2.0*KneeLink*FootLink)))*180/M_PI;
+
+  return angles;
+}
+
 
 void neutral(){
   setServoAngle(FLFoot, 0);
@@ -44,7 +81,6 @@ void setup() {
   pwm.setPWMFreq(50); 
   delay(10);
   
-  delay(2000); 
 }
 
 void loop() {
